65083be566e20c482253891670f40e65fcf46b60
Ponchai@PC <ponchai.a@aetriahealth.com>
2023-10-13 01:28:47 +0700

    Update strava connect

--- controllers/stravaController.js
+++ controllers/stravaController.js
@@ -1,7 +1,8 @@
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
-module.exports = async (req, res) => {    
+module.exports = async (req, res) => {
+    const refreshTokenUrl = `https://www.strava.com/oauth/authorize?client_id=${process.env.STRAVA_CLIENT_ID}&redirect_uri=${process.env.HOST}/exchangetoken&response_type=code&scope=activity:read_all`
     console.log('strava req', req)
     console.log('strava req user id', req.session.userId)
     let UserData = await prisma.users.findUnique({
@@ -11,7 +12,7 @@ module.exports = async (req, res) => {
     })
     let strava = await prisma.stravaProfiles.findFirst({
         where: {
-            userId: UserData.id,
+            userId: req.session.userId,
         },
     })
 
@@ -19,5 +20,6 @@ module.exports = async (req, res) => {
     res.render('strava', {
         UserData,
         strava,
+        refreshTokenUrl,
     })
 }
