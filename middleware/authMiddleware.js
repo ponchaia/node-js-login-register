b6c1490e3e1e4d459512be57c688aa68e88efdc4
Ponchai@PC <ponchai.a@aetriahealth.com>
2023-10-16 17:57:20 +0700

    .

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -10,7 +10,7 @@ module.exports = async (req, res, next) => {
         return res.redirect('/')
     })
     if (!user) {
-        return res.redirect('/')
+        return res.redirect('/login')
     }
     console.log('User logged in successfully')
     next()
